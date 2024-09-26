4c370b8cc8c1089436093ef9ff1f0e54ae991cf9
Obla <109748251+ivankypers@users.noreply.github.com>
2024-09-27 00:02:11 +0300

    Бургер меню

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+"use client"
+
+import React, {useState} from 'react';
 import styles from './Header.module.scss';
+import clsx from 'clsx';
 
 const Header: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+    const handleBurgerClick = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+
     return (
         <header className={styles.header}>
             <div className="container">
@@ -16,7 +26,24 @@ const Header: React.FC = () => {
                         <a className={styles.link} href="">Как работаем</a>
                     </li>
                 </ul>
+                <button
+                    className={clsx(styles.burger, {[styles.burgerActive]: menuOpen})}
+                    onClick={handleBurgerClick}
+                >
+                    <span className={styles.burgerLine}></span>
+                </button>
             </div>
+            <div className={clsx(styles.menu, {[styles.menuOpen]: menuOpen})}>
+                <ul>
+                    <li className={styles.item}>
+                        <a className={styles.link} href="">Преимущества</a>
+                    </li>
+                    <li className={styles.item}>
+                        <a className={styles.link} href="">Как работаем</a>
+                    </li>
+                </ul>
+            </div>
+
         </header>
     );
 };
